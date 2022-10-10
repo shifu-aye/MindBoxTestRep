0b45f4b31ec17f4c3b5b61254535dc8ab5fb11d9
alexey <aleshka.karnaukh@mail.ru>
2022-10-10 22:20:22 +0300

    First commit

---
+++ SquareLib/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareLib
+{
+    public class Area
+    {
+        public double Pi = 3.14;
+        public double Radius { get; set; }
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+        public double CircleАrea(double radius)
+        {
+            Radius = radius;
+            double area = Pi * Math.Pow(radius, 2);
+            return area;
+        }
+        public double TriangleАrea(double side1, double side2, double side3)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+            double HalfPerimetr = (side1 + side2 + side3) / 2;
+            double area = Math.Sqrt(HalfPerimetr * (HalfPerimetr - side1) * (HalfPerimetr - side2) * HalfPerimetr - side3);
+            return area;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Diagnostics;
+using WebApplication1.Models;
+using SquareLib;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(double RadiusTB)
+        {
+            try
+            {
+                Area area = new Area();
+                ViewBag.CircleArea = area.CircleАrea(RadiusTB);
+                return View(area);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                return View();
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult Index2(double Side1TB, double Side2TB, double Side3TB)
+        {
+            try
+            {
+                Area area = new Area();
+                ViewBag.TriangleArea = area.TriangleАrea(Side1TB, Side2TB, Side3TB);
+                return View(area);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                return View();
+            }    
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
